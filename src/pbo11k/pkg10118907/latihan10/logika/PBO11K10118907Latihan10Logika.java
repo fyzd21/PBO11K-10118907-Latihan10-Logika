5e080ae2e88123196116792aa3629aed7d17ee1c
unknown <fadlullahyazid@gmail.com>
2019-09-24 23:25:06 +0700

    First commit

---
+++ src/pbo11k/pkg10118907/latihan10/logika/PBO11K10118907Latihan10Logika.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11k.pkg10118907.latihan10.logika;
+
+/**
+ *
+ * @author
+ * NAMA     : Yazid Fadlullah At-Taukily
+ * KELAS    : PBO11K
+ * NIM      : 10118907
+ * Deskripsi Program : Latihan logika
+ * 
+ */
+public class PBO11K10118907Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status dewasa: "+dewasa);
+    }
+    
+}
